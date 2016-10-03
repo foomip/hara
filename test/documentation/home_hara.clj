ef3a52a2f52aae6b58c98b34250e73b9d2ec6113
Chris Zheng <z@caudate.me>
2016-10-03 22:04:43 +0800

    added home page

--- test/documentation/home_hara.clj
+++ test/documentation/home_hara.clj
@@ -1,3 +1,65 @@
 (ns documentation.home-hara)
 
-"Welcome to HARA"
+
+[[:image {:src "img/logo.png" :style "float: right"}]]
+
+
+"
+# Welcome
+
+### What is It?
+
+`hara` is a set of utilities and libraries extending `clojure.core`, a little bit like [boost](http://www.boost.org) but without the compilation time.
+
+### Why was it made?
+
+Apart from [clojure.core](http://www.clojure.org) itself, the language lacks a big, monolithic, kitchen-sink type library. `hara` aims to fill that gap.
+
+### Who is this targeting?
+
+Clojure developers, and people that like pretty code.
+
+### When was this started?
+
+The first pieces of the current codebase was committed in 2012. Since then, the project has grown organically based on need and interest.
+
+### Where has it been used?
+
+In many places. Mostly to write libraries that keep this maintainable. 
+
+### How to begin?
+
+That's a good question. It really depends on what is being built. There's a fair amount of documentation already so please jump in and start exploring.
+
+
+### Okay, lets go!
+
+List of current libraries:
+
+- [hara.class](hara-class.html) - functions for reasoning about classes
+- [hara.common](hara-common.html) - primitives declarations and functions
+- [hara.component](hara-component.html) - constructing composable systems
+- [hara.concurrent](hara-concurrent.html) - methods and datastructures for concurrency
+- [hara.concurrent.ova](hara-concurrent.ova.html) - shared mutable state
+- [hara.concurrent.procedure](hara-concurrent.procedure.html) - controllable execution
+- [hara.data](hara-data.html) - maps and representations of data
+- [hara.event](hara-event.html) - event signalling and conditional restart
+- [hara.expression](hara-expression.html) - interchange between code and data
+- [hara.extend](hara-extend.html) - macros for extensible objects
+- [hara.function](hara-function.html) - reasoning about functions
+- [hara.group](hara-group.html) - generic typed collections
+- [hara.io](hara-io.html) - tools for files and io operations
+- [hara.io.file](hara-io.file.html) - tools for the file system
+- [hara.io.scheduler](hara-io.scheduler.html) - easy and intuitive task scheduling
+- [hara.io.watch](hara-io.watch.html) - watch for filesystem changes
+- [hara.namespace](hara-namespace.html) - utilities for namespace manipulation
+- [hara.object](hara-object.html) - think data, escape encapsulation
+- [hara.reflect](hara-reflect.html) - java reflection made easy
+- [hara.sort](hara-sort.html) - micellaneous sorting functions
+- [hara.string](hara-string.html) - methods for string manipulation
+- [hara.test](hara-test.html) - easy to use test framework
+- [hara.time](hara-time.html) - time as a clojure map
+- [hara.zip](hara-zip.html) - data traversal in style
+"
+
+
