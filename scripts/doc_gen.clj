4e0da432c9ac7631de4f7f898092d628505deb62
Chris Zheng <z@caudate.me>
2016-10-05 23:14:30 +0800

    minor template change

--- scripts/doc_gen.clj
+++ scripts/doc_gen.clj
@@ -1,5 +1,5 @@
 (ns scripts.doc-gen
-  (:require [lucid.test :as lucid]
+  (:require [lucid.unit :as lucid]
             [lucid.publish :as publish]
             [lucid.publish.theme :as theme]
             [hara.io.project :as project]))
@@ -13,7 +13,7 @@
   (lucid/import 'hara.zip PROJECT)
   (lucid/import :all PROJECT)
   
-  (publish/copy-assets  PROJECT)
+  (publish/copy-assets (publish/load-settings {:refresh true} PROJECT) PROJECT)
 
   (doseq [key (->> PROJECT :publish :files keys sort)] 
     (println "PUBLISHING:" key)
@@ -22,7 +22,7 @@
   (publish/publish "hara-reflect" {:refresh true} PROJECT)
   (publish/publish "hara-zip" {} PROJECT)
   (publish/publish "hara-data" {} PROJECT)
-  (publish/publish "index" {:refresh true} PROJECT)
+  (publish/publish "index" {} PROJECT)
   
   (doseq [m (map second (-> PROJECT
                             :publish
